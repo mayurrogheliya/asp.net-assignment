47507bee32f59abc754da2b40c834783f1cf2e44
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-13 00:09:34 +0530

    assignment 2.3  is completed

---
+++ Assignment2/assignment23.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="assignment23.aspx.cs" Inherits="WebApplication1.Assignment2.assignment23" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+</asp:Content>
---
+++ Assignment2/assignment23.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Assignment2
+{
+    public partial class assignment23 : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string FileName = FileUpload1.FileName;
+                string path = Server.MapPath("~/Assignment2/Images")+FileName;
+                FileUpload1.SaveAs(path);
+                Label1.Text = "File uploaded successfully..!";
+            }
+        }
+    }
+}
