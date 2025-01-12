4ba4cbc0707a03966088c277b15315b38a0a7efe
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-12 22:53:18 +0530

    assignment 2.2 Calendar is completed

---
+++ Assignment2/assignment22.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Assignment2.2" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="assignment22.aspx.cs" Inherits="WebApplication1.Assignment2.assignment22" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+</asp:Content>
---
+++ Assignment2/assignment22.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Assignment2
+{
+    public partial class assignment22 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Label1.Text = $"Selected Date:  {Calendar1.SelectedDate.ToString("dd-MM-yyyy")} {Calendar1.SelectedDate.DayOfWeek.ToString()}";
+        }
+    }
+}
